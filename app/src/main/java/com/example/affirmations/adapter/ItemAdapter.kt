f546a0732e8f6e3dc8582776299a63b0cbc86113
Maximiliano <lmax23072002@gmail.com>
2023-12-13 10:39:34 +0200

    commit itemadapter

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -14,25 +14,29 @@ class ItemAdapter(
     private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    // ViewHolder class to hold the views for each item in the RecyclerView
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
+    // Create a new ViewHolder when needed
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        // create a new view
+        // Inflate the layout for a list item and create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item, parent, false)
 
         return ItemViewHolder(adapterLayout)
     }
 
-
+    // Return the size of the dataset
     override fun getItemCount(): Int {
         return dataset.size
     }
 
+    // Bind data to the views in the ViewHolder
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
+        // Set the text of the TextView to the string resource from the dataset
         holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 }
